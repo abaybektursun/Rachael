ffb9c434972dca0c94bb1cb370307cac1ab4cd5d
Abay Bektursun <abay.bektursun@hpe.com>
2016-11-24 15:52:08 -0600

    Contacts nodesList

--- Source/application/out/production/JavaFxApplication/src/client/ContactsController.java
+++ Source/application/out/production/JavaFxApplication/src/client/ContactsController.java
@@ -1,8 +1,13 @@
 package client;
 
-
+import com.jfoenix.controls.JFXNodesList;
+import javafx.animation.Interpolator;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -13,6 +18,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.BackgroundFill;
+import javafx.animation.KeyValue;
 
 import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXButton;
@@ -20,6 +26,7 @@ import com.jfoenix.controls.JFXListView;
 
 import javafx.event.ActionEvent;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -29,28 +36,75 @@ public class ContactsController implements Initializable {
     JFXListView<Label> listView;
 
     @FXML
-    JFXPopup popup;
+    JFXNodesList nodesListOptions;
+
+    @FXML
+    AnchorPane mainPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
+        mainPane.setStyle("-fx-background-color:WHITE");
         for (int i = 0; i < 4; i++){
             try{
                 Label lbl = new Label("Item " + i);
                 listView.getItems().add(lbl);
             }catch (Exception exc){ exc.printStackTrace();}
         }
+
+        listView.getStyleClass().add("mylistview");
+        listView.depthProperty().set(1);
+        //listView.setExpanded(true);
+
+        // Node list Buttons
+        JFXButton sbutton1 = new JFXButton();
+        sbutton1.setTooltip(new Tooltip("Options"));
+        Label slabel = new Label("Click");
+        slabel.setFont(new Font("Arial", 12));
+        slabel.setStyle("-fx-text-fill:WHITE");
+        sbutton1.setGraphic(slabel);
+        sbutton1.setButtonType(JFXButton.ButtonType.RAISED);
+        sbutton1.getStyleClass().addAll("animated-option-button","animated-option-sub-button");
+
+        JFXButton sbutton2 = new JFXButton();
+        Label slabel2 = new Label("Toggle");
+        slabel2.setFont(new Font("Arial", 10));
+        sbutton2.setGraphic(slabel2);
+        sbutton2.setTooltip(new Tooltip("Will create margins between contact cards"));
+        sbutton2.setButtonType(JFXButton.ButtonType.RAISED);
+        sbutton2.getStyleClass().addAll("animated-option-button");
+
+        sbutton2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                expand();
+            }
+        });
+
+        JFXButton sbutton3 = new JFXButton("Off");
+        sbutton3.setTooltip(new Tooltip("Log Off"));
+        sbutton3.setButtonType(JFXButton.ButtonType.RAISED);
+        sbutton3.getStyleClass().addAll("animated-option-button","animated-option-sub-button2");
+
+        //JFXNodesList nodesListOptions = new JFXNodesList();
+        nodesListOptions.setSpacing(6);
+        // init nodes
+        nodesListOptions.addAnimatedNode(sbutton1, (expanded)->{ return new ArrayList<KeyValue>(){{ add(new KeyValue(slabel.rotateProperty(), expanded? 360:0 , Interpolator.EASE_BOTH));}};});
+        nodesListOptions.addAnimatedNode(sbutton2);
+        nodesListOptions.addAnimatedNode(sbutton3);
+        nodesListOptions.setRotate(90);
+
+
     }
 
     @FXML
-    private void expand(ActionEvent event) {
+    private void expand() {
         if (!listView.isExpanded()) {
-            listView.setExpanded(true);
             listView.depthProperty().set(1);
+            listView.setExpanded(true);
         }
         else{
-            listView.setExpanded(true);
-            listView.depthProperty().set(0);
+            listView.depthProperty().set(1);
+            listView.setExpanded(false);
         }
     }
 
